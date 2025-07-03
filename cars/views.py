89db9a1b28e586467bae46b63a63ce0da20c5801
Renan <Renanoronha2003@gmail.com>
2025-07-03 10:14:33 -0300

    Adiciona arquivos e pastas iniciais do projeto

---
+++ cars/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from cars.models import Car
+
+
+def cars_view(request):
+    cars= Car.objects.all()
+    search = request.GET.get('search')
+    
+    if search:
+        cars = Car.objects.filter(model__contains=search)
+    
+    return render(
+        request,
+        'cars.html',
+        {'cars': cars}
+    )
