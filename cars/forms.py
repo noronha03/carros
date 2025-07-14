75b56c09b8c72c597d9324e95edfd2ba1e75f06a
Renan <Renanoronha2003@gmail.com>
2025-07-14 12:37:46 -0300

    teste

---
+++ cars/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+from cars.models import Brand, Car
+
+   
+class CarModelFrom(forms.ModelForm):
+    class Meta:
+        model = Car
+        fields = '__all__'
+
+
+    def clean_value(self):
+        value = self.cleaned_data.get('value')
+        if value < 20000:
+            self.add_error('value', 'Valor mínimo do carro deve er de R$20.000')
+        return value
+    
+    def clean_factory_year(self):
+        factory_year = self.cleaned_data.get('factory_year')
+        if factory_year < 1975:
+            self.add_error('factory_year', 'Não é possível aceitar carros fabricados antes de 1975')
+        return factory_year
+
