ee603fe7d9e7694cc628d0617afc6405587c0698
Renan <Renanoronha2003@gmail.com>
2025-07-25 14:39:23 -0300

    Atualiza templates e views; adiciona accounts e nova imagem

--- cars/views.py
+++ cars/views.py
@@ -1,28 +1,32 @@
 from django.shortcuts import render, redirect
 from cars.models import Car
 from cars.forms import CarModelFrom
+from django.views import View
+from django.views.generic import ListView
+        
+        
+class CarsListView(ListView):# Esta view exibe uma lista de objetos do modelo Car usando o ListView do Django
+    model = Car  # Define o modelo que será listado
+    template_name = 'cars.html'  # Define o template que será renderizado
+    context_object_name = 'cars'  # Nome da variável usada no template para acessar a lista
 
+    def get_queryset(self):# Este método retorna a lista de objetos que será exibida
+        cars = super().get_queryset().order_by('model') # Primeiro, busca todos os carros e ordena pelo campo 'model'
+        search = self.request.GET.get('search')# Pega o valor do parâmetro 'search' na URL, se existir
+        if search: # Se o usuário buscou algo, filtra os carros pelo modelo
+            cars = cars.filter(model__icontains=search)   
+        return cars# Retorna a lista (filtrada ou não)
 
-def cars_view(request):
-    cars = Car.objects.all().order_by('model')
-    search = request.GET.get('search')
+class NewCarView(View):
     
-    if search:
-        cars = Car.objects.filter(model__icontains=search)
+    def get(self, request):
+        new_car_form = CarModelFrom()
+        return render(request, 'new_car.html', {'new_car_form' : new_car_form})
     
-    return render(
-        request,
-        'cars.html',
-        {'cars': cars}
-    )
-
-
-def new_car_view(request):
-    if request.method == 'POST':
+    def post(self, request):
         new_car_form = CarModelFrom(request.POST, request.FILES)
         if new_car_form.is_valid():
             new_car_form.save()
             return redirect('cars_list')
-    else:
-        new_car_form = CarModelFrom()
-    return render(request, 'new_car.html', {'new_car_form' : new_car_form})
+        return render(request, 'new_car.html', {'new_car_form' : new_car_form})
+    
